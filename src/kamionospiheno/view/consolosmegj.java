f2dc512aa3ba044a591bc179882c3c7a110b969d
VorosDominik <medoxidhun@gmail.com>
2023-11-13 11:46:00 +0100

    alaphelyzet

---
+++ src/kamionospiheno/view/consolosmegj.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kamionospiheno.view;
+
+/**
+ *
+ * @author voros.dominik
+ */
+public class consolosmegj {
+    
+}
